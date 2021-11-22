8270a7e5f13b238ba730869f6399a31d1ac73999
Miran Jang <miranjang9507@gmail.com>
2021-11-22 13:50:41 -0500

    added authorization middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const Campground = require('../models/campground');
-const ExpressError = require('../utils/ExpressError');
-const { campgroundSchema } = require('../schemas');
-const { isLoggedIn } = require('../middleware');
+
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 const catchAsync = require('../utils/catchAsync');
 
-const validateCampground = (req, res, next) => {
-
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
 
 
 router.get('/', catchAsync(async (req, res, next) => {
@@ -41,8 +30,14 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 }));
 
 router.get('/:id', catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
-    console.log(campground)
+    const campground = await Campground.findById(req.params.id)
+        .populate({
+            path: 'reviews',
+            populate: {
+                path: 'author'
+            }
+        }).populate('author');
+    console.log(campground);
     if (!campground) {
         req.flash('error', 'Cannot find the campground');
         return res.redirect('/campgrounds')
@@ -50,19 +45,26 @@ router.get('/:id', catchAsync(async (req, res, next) => {
     res.render('campgrounds/show', { campground });
 }));
 
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res, next) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
+    const { id } = req.params;
+    if (!campground) {
+        req.flash('error', 'Cannot find the campground');
+        return res.redirect('/campgrounds')
+    }
+
     res.render('campgrounds/edit', { campground });
 }));
 
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res, next) => {
+
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Successfully updated a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res, next) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted a new campground!');
--- schemas.js
+++ schemas.js
@@ -18,3 +18,4 @@ module.exports.reviewSchema = Joi.object({
 
     }).required()
 });
+
